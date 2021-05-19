dd5feabcdcf083f0bd62249dbb02a346dc5d6d70
lruizrom <lruizrom@everis.com>
2021-05-19 09:01:12 +0200

    Añadidos cambios menores

--- src/operators/Pinball.java
+++ src/operators/Pinball.java
@@ -21,6 +21,7 @@ public class Pinball {
 		// Se comprueba que en la variable playerName entra un objeto de tipo String
 		if (playerName instanceof String) {
 			// Se selecciona la dificultad, cuanto más bajo el número, más fácil el juego y menos "GAME_OVER" hay
+
 			switch (difficulty) {
 			/*
 			 * La lógica del programa en los tres casos es igual: 1. Se rellena el array de posibilidades según la dificultad elegida 2. Se establecen las
@@ -28,8 +29,8 @@ public class Pinball {
 			 * (representa los rebotes que lleva en la partida) 3. Se llama al método startPlaying(): La pelota se pone en juego y se elige una posición
 			 * aleatoria del array hasta que la posición sea GAME OVER
 			 * 
-			 * INFO ADICIONAL: Si la pelota cae en GAME OVER en la primera tirada, se le suma un punto de consolación y se sigue jugando. 
-			 * Si se consiguen más de 1000 puntos en un juego, se te felicita
+			 * INFO ADICIONAL: Si la pelota cae en GAME OVER en la primera tirada, se le suma un punto de consolación y se sigue jugando. Si se consiguen más de
+			 * 1000 puntos en un juego, se te felicita
 			 */
 			case (1):
 				possiblePoints[0] = BUMPER;
@@ -93,41 +94,57 @@ public class Pinball {
 	}
 
 	private void startPlaying(String playerName) {
-		// Creamos dos objetos StringBuilder para los mensajes de la aplicación
+		// Creamos tres objetos StringBuilder para los mensajes de la aplicación
 		StringBuilder strGameOver = new StringBuilder();
-		strGameOver.append("La puntuación final de ");
-		strGameOver.append(playerName);
-		strGameOver.append(" fue: ");
-		
 		StringBuilder strCurrentPoints = new StringBuilder();
+		StringBuilder strLivesLeft = new StringBuilder();
 		
-		while (playing == true) {
-			int nextMove = (int) (Math.random() * 10 - 1);
-			// Lógica para que si la pelota cae en Game Over y no es la primera tirada, se acabe la partida
-			if (possiblePoints[nextMove] < 0 && bounces != 0) {
-				strGameOver.append(score);
-				strGameOver.append(" puntos");
-				System.out.println(strGameOver);
-				String mensajeFinal = (score > 1000) ? "¡Enhorabuena! ¡Has conseguido más de 1000 puntos!" : "Más suerte la próxima vez...";
-				System.out.println(mensajeFinal);
-				playing = false;
-			} else {
-				// Primero se comprueba que la primera vez que la pelota cae, no sea en game over
-				if (bounces == 0 && possiblePoints[nextMove] == -1) {
-					bounces++;
-					this.score += 1;
-					System.out.println("La puntuación actual es de 1 punto de consolación.");
-				// A partir de aquí la pelota puede caer en cualquier posición del pinball
+		// Se crea un bucle for para representar que hay 3 vidas para jugar
+		for (int i = 1; i < 4; i++) {
+			// Se obtienen las vidas que quedan aprovechando la variable i del for
+			int lives = 3 - i;
+			while (playing == true) {
+				int nextMove = (int) (Math.random() * 10 - 1);
+				// Lógica para cuando acaba la partida, en la que se dan los mensajes al jugador
+				if (possiblePoints[nextMove] < 0 && bounces != 0) {
+					// Construcción del mensaje de puntuación 
+					strGameOver.append("La puntuación final de ");
+					strGameOver.append(playerName);
+					strGameOver.append(" fue: ");
+					strGameOver.append(score);
+					strGameOver.append(" puntos");
+					strGameOver.setLength(0);
+					System.out.println(strGameOver);
+					// Si se consiguen más de 1000 puntos, el programa te felicita 
+					String mensajeFinal = (score > 1000) ? "¡Enhorabuena! ¡Has conseguido más de 1000 puntos!" : "Más suerte la próxima vez...";
+					System.out.println(mensajeFinal);
+					System.out.println();
+					playing = false;
+					// Construcción del mensaje de las vidas restantes
+					strLivesLeft.append("Te quedan ");
+					strLivesLeft.append(lives);
+					strLivesLeft.append(" vidas.\n");
+					System.out.println(strLivesLeft);
+					strLivesLeft.setLength(0);
 				} else {
-					bounces++;
-					this.score += possiblePoints[nextMove];
-					strCurrentPoints.append("La puntuacion actual es de: ");
-					strCurrentPoints.append(score);
-					strCurrentPoints.append(" puntos.");
-					System.out.println(strCurrentPoints);
-					strCurrentPoints.setLength(0);
+					// Primero se comprueba que la primera vez que la pelota cae, no sea en game over
+					if (bounces == 0 && possiblePoints[nextMove] == -1) {
+						bounces++;
+						this.score += 1;
+						System.out.println("La puntuación actual es de 1 punto de consolación.");
+						// A partir de aquí la pelota puede caer en cualquier posición del pinball
+					} else {
+						bounces++;
+						this.score += possiblePoints[nextMove];
+						strCurrentPoints.append("La puntuacion actual es de: ");
+						strCurrentPoints.append(score);
+						strCurrentPoints.append(" puntos.");
+						System.out.println(strCurrentPoints);
+						strCurrentPoints.setLength(0);
+					}
 				}
 			}
+			this.playing = true;
 		}
 	}
 }
